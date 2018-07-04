bf1d9a52c779e7b21fc51c604591f82fa2c4876e
ttian99 <176553611@qq.com>
2018-07-04 10:16:29 +0800

    no message

--- assets/Script/2048/gameOverLayer.ts
+++ assets/Script/2048/gameOverLayer.ts
@@ -6,8 +6,9 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class GameOverLayer extends cc.Component {
   @property(cc.Prefab) CoreBg = null;
+  @property(cc.Label) scoreLabel = null;
   start() {
-
+    this.scoreLabel = `得分: ${gameCtrl.score}`;
   }
   clickReplayBtn() {
     this.node.destroy();
